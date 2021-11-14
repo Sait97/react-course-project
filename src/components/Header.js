3eca92db4a9ca03fdf61c9177eb71612b8b9657d
Sait97 <44225063+Sait97@users.noreply.github.com>
2021-11-14 18:19:35 +0200

    finish header

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,22 +5,40 @@ const Header = () => {
 
     <header id="header" >
         <div className="fixed-top row0">
-            <div className="container">
+            <div className="container d-flex">
 
-                <div className="social-links float-left">
-                    <a href="#" className="twitter"><i className="fa fa-twitter"></i></a>
+                <div className="social-links  d-flex col-4">
+                   
                     <a href="#" className="facebook"><i className="fa fa-facebook"></i></a>
                     <a href="#" className="instagram"><i className="fa fa-instagram"></i></a>
                     <a href="#" className="google-plus"><i className="fa fa-google-plus"></i></a>
                     <a href="#" className="linkedin"><i className="fa fa-linkedin"></i></a>
                 </div>
 
-                <div className="logo float-center">
+                <div className="logo d-flex col-6">
 
                     <h1 className="text-light"><a href="#header"><span>Lu<span className="orange-color">xo</span>ry Wa<span className="orange-color">tch</span>es</span></a></h1>
                     <a href="#intro" className="scrollto"><img src="img/logo.png" alt="" className="img-fluid" /></a>
                 </div>
 
+                <div className="card-links d-flex col-2">
+                <ul className="main-nav  d-flex">
+                    <li>
+
+                    <a href="" className="bordered-icon"><i className="fa fa-heart"></i></a>
+                    </li>
+                    <li>
+                    <div className="d-flex ">
+                        <a className="mini-cart__dropdown-toggle bordered-icon d-flex" id="cartDropdown">
+                        <span className="mini-cart__count">0</span>
+                            <i class="ion-ios-cart-outline mini-cart__icon"></i>
+                        <span className="mini-cart__ammount">80.00</span>
+                         {/* <i className="fa fa-angle-down absolute orange-color"></i> */}
+                        </a>
+                    </div>
+                        </li>
+                </ul>
+                </div>
                
 
             </div>
